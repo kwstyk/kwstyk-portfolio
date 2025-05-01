826f0278d946208ccb34080ec031f385b6319c10
Yuki Kawasato <yuki1103@ymail.ne.jp>
2025-05-02 00:58:49 +0900

    fix: correct ESLint plugin setup for Astro

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 // eslint.config.js
 import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
 import astro from "eslint-plugin-astro";
 
 export default [
   js.configs.recommended,
   {
     files: ["**/*.astro"],
-    plugins: { astro },
     languageOptions: {
       parser: astro.parsers["astro"],
     },
+    plugins: {
+      astro,
+    },
     rules: {
-      // 必要に応じて
+      // 必要ならカスタムルールを追加
     },
   },
-  prettier,
 ];
