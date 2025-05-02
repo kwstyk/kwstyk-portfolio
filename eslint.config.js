5b9a67813ca89584683e0dbc644e5d1d64b5de5f
Yuki Kawasato <yuki1103@ymail.ne.jp>
2025-05-02 23:18:36 +0900

    Fix JSX parent element issue in ProofList.astro

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,24 @@
-// eslint.config.js  ─ PowerShell の >`/>` は不要、そのまま上書き
-import js            from '@eslint/js';
-import astro         from 'eslint-plugin-astro';
-import astroParser   from 'astro-eslint-parser';
-import tsParser      from '@typescript-eslint/parser';
-import tsPlugin      from '@typescript-eslint/eslint-plugin';
-import prettier      from 'eslint-config-prettier';
+// eslint.config.js  ― FlatConfig
+import astro          from 'eslint-plugin-astro';
+import astroParser    from 'astro-eslint-parser';
+import tsParser       from '@typescript-eslint/parser';
+import tsPlugin       from '@typescript-eslint/eslint-plugin';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  js.configs.recommended,
-
-  // --- Astro ファイル (.astro) ---------------------------------
   {
     files: ['**/*.astro'],
     languageOptions: {
       parser: astroParser,
-      parserOptions: { parser: tsParser, extraFileExtensions: ['.astro'] },
+      parserOptions: { parser: tsParser, extraFileExtensions: ['.astro'] }
     },
     plugins: { astro },
-    rules: {
-      'astro/no-unused-css-selector': 'warn',
-    },
+    rules: { 'astro/no-unused-css-selector': 'warn' }
   },
-
-  // --- TypeScript / JS -----------------------------------------
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+    files: ['**/*.{ts,js}'],
     languageOptions: { parser: tsParser },
     plugins: { '@typescript-eslint': tsPlugin },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-    },
-  },
-
-  // --- Prettier  ------------------------------------------------
-  prettier,
+    rules: { '@typescript-eslint/no-unused-vars': 'warn' }
+  }
 ];
